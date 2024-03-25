89a8ca66dd24b96341cb8f28a7a44202aa32e8b8
picklerick213 <164865394+picklerick213@users.noreply.github.com>
2024-03-26 03:28:10 +0800

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -39,6 +39,19 @@ export const authOptions = {
           }
         })
   ],
+};
+
+export async function isAdmin() {
+  const session = await getServerSession(authOptions);
+  const userEmail = session?.user?.email;
+  if (!userEmail) {
+    return false;
+  }
+  const userInfo = await UserInfo.findOne({email:userEmail});
+  if (!userInfo) {
+    return false;
+  }
+  return userInfo.admin;
 }
 
 
