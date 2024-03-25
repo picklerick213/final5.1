9e0582674c010c4f46635ad1b770b7f5503cc395
picklerick213 <164865394+picklerick213@users.noreply.github.com>
2024-03-26 04:04:25 +0800

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,12 @@
+import clientPromise from "@/libs/mongoConnect";
+import {UserInfo} from "@/models/UserInfo";
 import bcrypt from "bcrypt";
 import * as mongoose from "mongoose";
-import { User } from '@/models/User';
-import NextAuth, { getServerSession } from "next-auth";
+import {User} from '@/models/User';
+import NextAuth, {getServerSession} from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "@/libs/mongoConnect";
-import { UserInfo } from "../../../../models/UserInfo";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 
 export const authOptions = {
   secret: process.env.SECRET,
@@ -17,44 +17,43 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     CredentialsProvider({
-      name: "Credentials",
-      id: "credentials",
+      name: 'Credentials',
+      id: 'credentials',
       credentials: {
         username: { label: "Email", type: "email", placeholder: "test@example.com" },
-        password: { label: "Password", type: "password", placeholder: "password" },
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
         const email = credentials?.email;
         const password = credentials?.password;
 
-        // Connect to MongoDB within the authorize function
-        await mongoose.connect(process.env.MONGO_URL);
-
-        const user = await User.findOne({ email });
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({email});
         const passwordOk = user && bcrypt.compareSync(password, user.password);
 
         if (passwordOk) {
           return user;
         }
 
-        return null;
-      },
-    }),
+        return null
+      }
+    })
   ],
 };
 
-export async function isAdmin(req) { // Make isAdmin accessible through req object
-  const session = await getServerSession(authOptions, req); // Pass req to getServerSession
+export async function isAdmin() {
+  const session = await getServerSession(authOptions);
   const userEmail = session?.user?.email;
   if (!userEmail) {
     return false;
   }
-  const userInfo = await UserInfo.findOne({ email: userEmail });
+  const userInfo = await UserInfo.findOne({email:userEmail});
   if (!userInfo) {
     return false;
   }
   return userInfo.admin;
 }
 
-// Use default export for API route handler
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST }
