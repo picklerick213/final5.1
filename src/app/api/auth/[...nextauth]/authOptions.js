8319e4527adc5a05150b3f7eec00b02d6daa68e1
picklerick213 <164865394+picklerick213@users.noreply.github.com>
2024-04-07 18:46:53 +0800

    Update authOptions.js

--- src/app/api/auth/[...nextauth]/authOptions.js
+++ src/app/api/auth/[...nextauth]/authOptions.js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { authOptions } from "next-auth/authOptions";
+import { authOptions } from "@/auth/authOptions";
 
 const handler = NextAuth(authOptions);
 
