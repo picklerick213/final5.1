8cc79bec247cbd16e383d97505d70523d1d708e1
Neil Albert Dela Cruz <143504592+WH20ter21@users.noreply.github.com>
2024-04-07 18:17:09 +0800

    Update route.js

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,6 +1,6 @@
 import { User } from "@/models/User";
 import mongoose from "mongoose";
-import { isAdmin } from "../auth/[...nextauth]/route";
+import { isAdmin } from "../auth/[...nextauth]/utils";
 
 
 
