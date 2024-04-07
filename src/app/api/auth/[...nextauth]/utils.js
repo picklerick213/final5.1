234be04c0b76d96b63bcff80d67b84faaacb0687
Neil Albert Dela Cruz <143504592+WH20ter21@users.noreply.github.com>
2024-04-07 18:14:50 +0800

    Update utils.js

--- src/app/api/auth/[...nextauth]/utils.js
+++ src/app/api/auth/[...nextauth]/utils.js
@@ -1,1 +1,16 @@
+import { getServerSession } from "next-auth";
+import { UserInfo } from "@/models/UserInfo";
+
+export async function isAdmin() {
+  const session = await getServerSession();
+  const userEmail = session?.user?.email;
+  if (!userEmail) {
+    return false;
+  }
+  const userInfo = await UserInfo.findOne({ email: userEmail });
+  if (!userInfo) {
+    return false;
+  }
+  return userInfo.admin;
+}
 
