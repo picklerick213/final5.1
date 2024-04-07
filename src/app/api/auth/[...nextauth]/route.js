b1886ac1dfd57b50a22a3375dc94b55b40b0b3f1
picklerick213 <164865394+picklerick213@users.noreply.github.com>
2024-04-07 18:20:52 +0800

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -41,19 +41,6 @@ export const authOptions = {
   ],
 };
 
-export async function isAdmin() {
-  const session = await getServerSession(authOptions);
-  const userEmail = session?.user?.email;
-  if (!userEmail) {
-    return false;
-  }
-  const userInfo = await UserInfo.findOne({email:userEmail});
-  if (!userInfo) {
-    return false;
-  }
-  return userInfo.admin;
-}
-
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
