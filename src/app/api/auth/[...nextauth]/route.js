ebf70e647b440d9b27a693c3c608d34d119e40db
Neil Albert Dela Cruz <143504592+WH20ter21@users.noreply.github.com>
2024-04-07 17:45:54 +0800

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -41,6 +41,19 @@ export const authOptions = {
   ],
 };
 
+export async function isAdmin() {
+  const session = await getServerSession(authOptions);
+  const userEmail = session?.user?.email;
+  if (!userEmail) {
+    return false;
+  }
+  const userInfo = await UserInfo.findOne({email:userEmail});
+  if (!userInfo) {
+    return false;
+  }
+  return userInfo.admin;
+}
+
 export const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
