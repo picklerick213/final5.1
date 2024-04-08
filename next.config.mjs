53ac02ffa59169126c4b59c2fa360d186d6babc5
picklerick213 <164865394+picklerick213@users.noreply.github.com>
2024-04-08 19:59:48 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,5 +13,14 @@ const nextConfig = {
         ]
     }
 };
+module.exports = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      "mongodb-client-encryption": false ,
+      "aws4": false
+    };
+
+    return config;
+  }
 
 export default nextConfig;
